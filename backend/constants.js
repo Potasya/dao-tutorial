cef48c9e983ffe99f8d6d64b479cc83cb1466ec0
Potasya <yakovlevamava@gmail.com>
2023-02-16 16:36:08 +0100

    + initial commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x0dFA06cc5C8165ce4d15EC0869d1baF1D1916eEc";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
